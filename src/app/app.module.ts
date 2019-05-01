952fadb6541d401dbfbe649dbfc25a0cffeef6d1
Umair Farooq <umairfarooq@live.com>
2019-05-01 14:02:09 +0500

    Heroes component, uppercase pipe, ngModel and FormsModule import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
